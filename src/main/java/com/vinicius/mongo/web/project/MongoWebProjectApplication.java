89fc97ac9d291b8f712ea0aca4a2f0aabff1f391
Vinicius Pascucci <vinicius.pascucci1@gmail.com>
2021-01-27 19:49:41 -0300

    Primeiro Commit - Projeto com MongoDB Criado

---
+++ src/main/java/com/vinicius/mongo/web/project/MongoWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.mongo.web.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoWebProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinicius/mongo/web/project/MongoWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.mongo.web.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
